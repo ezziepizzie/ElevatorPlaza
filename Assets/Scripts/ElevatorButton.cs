cbb8c92c3c29e051ad64bad4588027355ed434de
ezziepizzie <the46beast@gmail.com>
2025-12-17 11:57:09 +0800

    added button sprite and anim, added background

--- Assets/Scripts/ElevatorButton.cs
+++ Assets/Scripts/ElevatorButton.cs
@@ -5,6 +5,7 @@ public class ElevatorButton : MonoBehaviour, IPointerClickHandler
 {
     public Elevator elevator;
     AudioManager audioManager;
+    [SerializeField] private Animator elevatorButtonAnim;
 
     private void Awake()
     {
@@ -15,7 +16,11 @@ public class ElevatorButton : MonoBehaviour, IPointerClickHandler
     {
         if (elevator.isActive == false) return;
 
+        if(elevator.currentCapacity == 0) return;
+
+        elevatorButtonAnim.SetBool("buttonClicked", false);
         elevator.StartCoroutine("MoveElevatorUp");
         audioManager.PlaySFX(audioManager.elevetorButtonPress);
+        elevatorButtonAnim.SetBool("buttonClicked", true);
     }
 }
